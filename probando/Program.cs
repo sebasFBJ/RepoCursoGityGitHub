8d126c09a943aceafbfd95903e7604e110e950cd
sebas <sebastiabravo3@gmail.com>
2025-08-25 15:02:16 -0500

    peeba de seguridad con github

--- probando/Program.cs
+++ probando/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! , desde .NET");
+
+string testingConection = "server=localhost;database=master;user=sa;password=Your_password123;TrustServerCertificate=True";
